c8983e839794508356bbeb69cbe48c496d58fb16
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2023-05-22 22:21:34 -0300

    feat: create use-case register

---
+++ src/use-cases/register.ts
@@ -0,0 +1,39 @@
+import { User } from '@prisma/client'
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface IRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: User
+}
+
+export class RegisterUseCase {
+  public async execute({
+    name,
+    email,
+    password,
+  }: IRequest): Promise<IResponse> {
+    const prismaUsersRepository = new PrismaUsersRepository()
+
+    const hashedPassword = await hash(password, 8)
+
+    const hasEmail = await prismaUsersRepository.findByEmail(email)
+
+    if (hasEmail) {
+      return new Error('Email already exists')
+    }
+
+    const user = await prismaUsersRepository.create({
+      name,
+      email,
+      password_hash: hashedPassword,
+    })
+
+    return user
+  }
+}
