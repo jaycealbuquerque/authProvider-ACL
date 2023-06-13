b195759e86768ef8751fd59cc35808ce0cd226d0
“jaycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2023-06-13 19:43:56 -0300

    feat: delete create role on permission

--- src/http/controller/access-control-lists/routes.ts
+++ src/http/controller/access-control-lists/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { CreateRoleController } from './create-role'
 import { CreatePermissionController } from './create-permission'
-import { CreateRolePermissionController } from './create-role-permission'
 import { CreateUserAccessControllistController } from './create-user-access-control-list'
 import { is } from '../../middlewares/permissions'
 import { ensureAuthenticated } from '../../middlewares/ ensureAuthenticated'
@@ -14,11 +13,6 @@ accessControlRoutes.post(
   new CreateRoleController().handle,
 )
 
-accessControlRoutes.post(
-  '/role/:roleId',
-  ensureAuthenticated,
-  new CreateRolePermissionController().handle,
-)
 accessControlRoutes.post(
   '/permission',
   ensureAuthenticated,
